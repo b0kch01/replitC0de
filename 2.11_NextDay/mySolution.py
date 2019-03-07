70e33f6c0b28874e1c9670d10d5d7d7d6fc83686
B0kCh01 <44041512+B0kCh01@users.noreply.github.com>
2019-03-07 08:40:37 -0800

    Update mySolution.py

--- 2.11_NextDay/mySolution.py
+++ 2.11_NextDay/mySolution.py
@@ -1,8 +1,12 @@
-max_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
 def next_day(month, day):
-    if(month == 12 and day == 31):
-        return "1/1"
-    if(day == max_days[month]):
-       return("{}/1".format(month + 1))
-    return("{}/{}".format((month), (day + 1)))
+  months_with_30_days = [4, 6, 9, 11]
+  months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
+  day = day + 1
+  if month == 12 and day == 32:
+    month = 1
+    day = 1
+  elif (month == 2 and day == 29) or (month in months_with_30_days and day == 31) or (month in months_with_31_days and day == 32):
+    month = month + 1 
+    day = 1
+  return str(month) + "/" + str(day)
+
