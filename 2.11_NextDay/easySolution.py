0f37ac902d1002f6b5d269bd802d9c9747d1ddd0
B0kCh01 <44041512+B0kCh01@users.noreply.github.com>
2019-03-07 08:44:21 -0800

    Update easySolution.py

--- 2.11_NextDay/easySolution.py
+++ 2.11_NextDay/easySolution.py
@@ -1,10 +1,10 @@
 def next_day(month, day):
-  30_months = [4, 6, 9, 11]
-  31_months = [1, 3, 5, 7, 8, 10, 12] 
+  months_30 = [4, 6, 9, 11]
+  months_31 = [1, 3, 5, 7, 8, 10, 12] 
   if month == 12 and day == 31:
     month = 1
     day = 1
-  elif (month == 2 and day == 28) or (30_months and day == 30) or (31_months and day == 31):
+  elif (month == 2 and day == 28) or (months_30 and day == 30) or (months_31 and day == 31):
     month = month + 1 
     day = 1
   return str(month) + "/" + str(day)
