adff4ef96078211671aa7a5c882459d16cdb2345
B0kCh01 <44041512+B0kCh01@users.noreply.github.com>
2019-04-19 08:11:08 -0700

    Update RegularCode.py

--- 3.6 Caesar Cipher/RegularCode.py
+++ 3.6 Caesar Cipher/RegularCode.py
@@ -3,7 +3,9 @@ def caesarShift(text, n):
     for i in range(0, len(text)):
         letter = text[i]
         alphabet = ord(letter.upper()) + n
-        if not(alphabet < 65 or alphabet > 90):
+        if ord(letter) > 90 or ord(letter) < 65:
+            output += letter
+        eliff not(alphabet < 65 or alphabet > 90):
             output += chr(alphabet) if letter == letter.upper() else chr(alphabet).lower()
         else:
             output += chr(alphabet - 26) if letter == letter.upper() else chr(alphabet - 26).lower()
